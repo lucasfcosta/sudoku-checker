4166774fd53f48a495403142dffc5eccb4ef0e55
lucasfcosta <fernandesdacostalucas@gmail.com>
2015-10-05 21:41:21 -0300

    Cleaning and commenting code

--- sudoku-base.c
+++ sudoku-base.c
@@ -29,24 +29,31 @@ int load_grid(int grid[][SIZE], char *filename) {
 
 
 int verifica(int x, int max_x, int y, int max_y) {
+	// Array de números verificados
+	// Ao encontrar um número marca 1 no índice dele
 	int verificados[SIZE] = { 0 };
 
-
+	// Percorre da linha X até a linha MAX_X
 	for(int i = x; i < max_x; ++i) {
+		// Percorre da linha Y até a coluna MAX_Y	
 		for(int j = y; j < max_y; ++j) {
+			// Pega o número atual na posição X, Y
 			int numero = grid[i][j];
 			if(verificados[numero-1]) {
+				// Se o índice igual a esse número já estiver marcado incrementa erros e retorna
 				pthread_mutex_lock(&mutex_erro);
 				++erro;
 				pthread_mutex_unlock(&mutex_erro);
 				return 0;
 			} else {
+				// Se o índice igual ao número não estava marcado com 1, marca
 				verificados[numero-1] = 1;
 			}
 		}
 
 	}
 
+	// Observação: fazemos numero-1 na posição porque os números vão de 1 a 9 e nosso array vai de 0 a 8
 	return 1;
 }
 
@@ -68,7 +75,6 @@ void * main_loop(void * param) {
 		
 		switch(tarefa) {
 			case 0:
-				//
 				if(!verifica(0, SIZE, numero, numero)) {
 					printf("Thread: %d - Erro na linha: %d\n", (int) param, numero);
 				}
@@ -76,7 +82,6 @@ void * main_loop(void * param) {
 				break;
 
 			case 1:
-				//
 				if(!verifica(numero, numero, 0, SIZE)) {
 					printf("Thread: %d - Erro na coluna: %d\n", (int) param, numero);
 				}
@@ -84,7 +89,6 @@ void * main_loop(void * param) {
 				break;
 
 			default:
-				//
 				linha_inicial = numero % 3;
 				coluna_inicial = numero / 3;
 
@@ -120,15 +124,11 @@ int main(int argc, char *argv[]) {
 		}
 		printf("\n");
 
-		// Número máximos de tasks para realizar é TASKS
-		// limita a esse número, o número de Threads
-		if(max_threads >= TASKS)		
-			max_threads = TASKS;
-
 		pthread_t threads[max_threads];
 		pthread_mutex_init(&mutex_erro, NULL);
 		pthread_mutex_init(&mutex_current, NULL);
 
+		// Cria o número de threads solicitadas
 		for(int i = 0; i < max_threads; ++i) {
             pthread_create(&threads[i], NULL, main_loop, (void *) i);
         }
